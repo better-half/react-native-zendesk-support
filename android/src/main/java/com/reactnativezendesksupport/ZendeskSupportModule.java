bb9895783727eb872e2c58c1cc4e60a1db5a5509
Tim Claes <tim.claes@paired.com>
2023-05-22 14:11:00 +0200

    chore: android implementation

--- android/src/main/java/com/reactnativezendesksupport/ZendeskSupportModule.java
+++ android/src/main/java/com/reactnativezendesksupport/ZendeskSupportModule.java
@@ -1,33 +1,114 @@
 package com.reactnativezendesksupport;
 
-import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
-import com.facebook.react.bridge.Promise;
+import androidx.annotation.RequiresApi;
+
+import zendesk.configurations.Configuration;
+import zendesk.core.Zendesk;
+import zendesk.core.Identity;
+import zendesk.core.JwtIdentity;
+import zendesk.core.AnonymousIdentity;
+import zendesk.support.Support;
+import zendesk.support.guide.HelpCenterActivity;
+import zendesk.support.request.RequestActivity;
+import zendesk.support.requestlist.RequestListActivity;
+
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.module.annotations.ReactModule;
 
-@ReactModule(name = ZendeskSupportModule.NAME)
-public class ZendeskSupportModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "ZendeskSupport";
+import java.util.ArrayList;
+import java.util.Locale;
 
-    public ZendeskSupportModule(ReactApplicationContext reactContext) {
+public class RNZendeskBridge extends ReactContextBaseJavaModule {
+
+    public RNZendeskBridge(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
     @Override
-    @NonNull
     public String getName() {
-        return NAME;
+        return "RNZendeskSupport";
     }
 
+    // MARK: - Initialization
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @ReactMethod
-    public void multiply(double a, double b, Promise promise) {
-        promise.resolve(a * b);
+    public void initialize(ReadableMap config) {
+        String appId = config.getString("appId");
+        String zendeskUrl = config.getString("zendeskUrl");
+        String clientId = config.getString("clientId");
+        Zendesk.INSTANCE.init(getReactApplicationContext(), zendeskUrl, appId, clientId);
+        Support.INSTANCE.init(Zendesk.INSTANCE);
     }
 
+    // MARK: - Indentification
+
+    @ReactMethod
+    public void identifyJWT(String token) {
+        JwtIdentity identity = new JwtIdentity(token);
+        Zendesk.INSTANCE.setIdentity(identity);
+    }
+
+    @ReactMethod
+    public void identifyAnonymous(String name, String email) {
+        Identity identity = new AnonymousIdentity.Builder()
+            .withNameIdentifier(name)
+            .withEmailIdentifier(email)
+            .build();
+
+        Zendesk.INSTANCE.setIdentity(identity);
+    }
+
+    // MARK: - UI Methods
+
+    @ReactMethod
+    public void showHelpCenter(ReadableMap options) {
+        Configuration hcConfig = HelpCenterActivity.builder()
+                .withContactUsButtonVisible(!(options.hasKey("hideContactSupport") && options.getBoolean("hideContactSupport")))
+                .config();
+
+        Intent intent = HelpCenterActivity.builder()
+                .withContactUsButtonVisible(true)
+                .intent(getReactApplicationContext(), hcConfig);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getReactApplicationContext().startActivity(intent);
+    }
+
+    @ReactMethod
+    public void setHelpCenterLocaleOverride(String locale) {
+        try {
+            Locale newLocale = new Locale.Builder().setLanguageTag(locale).build();
+            Support.INSTANCE.setHelpCenterLocaleOverride(newLocale);
+        } catch (Exception e) {
+            Log.e("Zendesk", "error setting locale " + locale, e);
+        }
+    }
+    
+    @ReactMethod
+    public void showNewTicket(ReadableMap options) {
+        ArrayList tags = options.getArray("tags").toArrayList();
+
+        Intent intent = RequestActivity.builder()
+                .withTags(tags)
+                .intent(getReactApplicationContext());
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getReactApplicationContext().startActivity(intent);
+    }
+
+    @ReactMethod
+    public void showTicketList() {
+        Intent intent = RequestListActivity.builder()
+                .intent(getReactApplicationContext());
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getReactApplicationContext().startActivity(intent);
+    }
 }
--- android/src/main/java/com/reactnativezendesksupport/ZendeskSupportPackage.java
+++ android/src/main/java/com/reactnativezendesksupport/ZendeskSupportPackage.java
@@ -1,28 +1,29 @@
 package com.reactnativezendesksupport;
 
-import androidx.annotation.NonNull;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.ReactApplicationContext;
 
+import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
-public class ZendeskSupportPackage implements ReactPackage {
-    @NonNull
+public class RNZendeskPackage implements ReactPackage {
+
     @Override
-    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(
+        ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new ZendeskSupportModule(reactContext));
+
+        modules.add(new RNZendeskBridge(reactContext));
         return modules;
     }
 
-    @NonNull
     @Override
-    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList();
     }
 }
